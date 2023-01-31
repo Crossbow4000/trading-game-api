4d94461a5d238ac8ec593d66664d6df6a55ba52d
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-31 10:43:43 -0600

    1 file modified

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ const key = require('./key.json')
 admin.initializeApp({
   credential: admin.credential.cert(key)
 });
+
 const firestore = admin.firestore()
+
+const usersCollection = firestore.collection('users')
 const itemsCollection = firestore.collection('items')
+const recipesCollection = firestore.collection('recipes')
 
 const app = require('express')()
+
+function CraftItem(uid, recipe) {
+  recipesCollection.get().then(recipiesSnapshot => {
+    const recipies = recipiesSnapshot.docs
+  })
+}
+
+app.get('/', (req, res) => {
+  const resObject = {
+    status: 404,
+    description: "This API endpoint does not exist"
+  }
+  res.send(JSON.stringify(resObject))
+})
+
+app.get('/CRAFT', (req, res) => {
+  var resObject = {
+    status: 200,
+    description: "The request was recieved and succesfully proccessed"
+  }
+  res.send(JSON.stringify(resObject))
+})
+
+app.listen(8000)
 
 module.exports = app
