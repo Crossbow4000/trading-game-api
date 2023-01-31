8411b60c98819139d989aa35495c3cb16dc9be70
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-30 21:10:09 -0600

    3 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-const firebase = require('firebase/compat/app')
-require('firebase/compat/auth')
+const admin = require('firebase-admin')
+const key = require('./key.json')
+admin.initializeApp({
+  credential: admin.credential.cert(key)
+});
+const firestore = admin.firestore()
+const itemsCollection = firestore.collection('items')
 
-const express = require('express')
-const app = express()
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBXM0NEzmyzAgM02cXGnX3-skmo4GL70-8",
-  authDomain: "trading--game.firebaseapp.com",
-  projectId: "trading--game",
-  storageBucket: "trading--game.appspot.com",
-  messagingSenderId: "980050881982",
-  appId: "1:980050881982:web:32247565ca0a9da18b9def",
-  measurementId: "G-JR8HQZG0QV"
-};
-firebase.initializeApp(firebaseConfig);
-
-app.get('/', (req, res) => {
-  firebase.auth().signInWithEmailAndPassword("root@trading-game.com", "c|G@-+tP@/KEa.a\qZ$/*UY]TiJf{@!i1vU(")
-  .then(userCredential => res.send(userCredential))
-})
-
-app.listen(8000, () => {
-
-})
+const app = require('express')()
 
 module.exports = app
