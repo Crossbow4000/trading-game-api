e9185caee413f8860da0f2011ac36c0bd7beef76
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-01-31 21:05:50 -0600

    1 file modified

--- index.js
+++ index.js
@@ -12,55 +12,6 @@ const recipesCollection = firestore.collection('recipes')
 
 const app = require('express')()
 
-const CraftItem = (uid, recipe) => {
-  usersCollection.get().then(usersSnapshot => {
-    recipesCollection.get().then(recipiesSnapshot => {
-
-
-      console.log(userSnapshot.docs)
-      const user = usersSnapshot.docs[uid]
-      const craftingRecipe = recipiesSnapshot.docs[recipe]
-
-      if(!user) {
-        return({
-          status: 400,
-          description: "Invalid uid parameter"
-        })
-      }
-      if(!craftingRecipe) {
-        return({
-          status: 400,
-          description: "Invalid recipe"
-        })
-      }
-
-      let newInventory = user.inventory;
-      for(let i = 0; i < newInventory.length; i++) {
-        newInventory[i] += craftingRecipe.recipe[i]
-      }
-
-      usersCollection.doc(uid).set({
-        inventory: newInventory
-      }, {merge: true})
-      .then(response => {
-        console.log(uid, recipe)
-        return({
-          status: 200,
-          description: "Request was recieved and processed"
-        })
-      })
-      .catch(error => {
-        return({
-          status: 500,
-          description: `There was an internal server error    :::    ${error}`
-        })
-      })
-
-
-    })
-  })
-}
-
 app.get('/', (req, res) => {
 
 
@@ -72,8 +23,8 @@ app.get('/', (req, res) => {
       recipesCollection.get().then(recipiesSnapshot => {
   
   
-        const user = usersSnapshot.data().docs[req.query.uid]
-        const craftingRecipe = recipiesSnapshot.data().docs[req.query.recipe]
+        const user = usersSnapshot.docs[req.query.uid]
+        const craftingRecipe = recipiesSnapshot.docs[req.query.recipe]
   
         if(!user) {
           console.log(usersSnapshot.docs)
