dbb065cc773bf2c8e5f743457ba2f01fdd99cddf
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-02-15 19:43:37 -0600

    1 file modified

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.get('/', (req, res) => {
             if(marketsSnapshot.docs.length < length) {
               for(i = 0; i < length - marketsSnapshot.docs.length; i++) {
                 markets.push(marketsSnapshot.docs.length + i - 1)
-                marketsCollection.doc(String(marketsSnapshot.docs.length + i - 1)).set({
+                marketsCollection.doc(String(marketsSnapshot.docs.length + i)).set({
                   defaultPrice: 0,
                   buys: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                   sells: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
